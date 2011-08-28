793749680046f349b9a77ae56d9c81598835adab
Alex Belozerov <alex@welldonecode.com>
2011-08-28 17:22:34 +0700

    Options

---
+++ settings.js
@@ -0,0 +1,11 @@
+var overlayUniqueId = "overlay_3985123731465987";
+var overlay = $('#'+overlayUniqueId);
+if(opacity!=false)
+    overlay.css('opacity',opacity);
+if(left!=false){
+    overlay.css('left',left+"px");
+}
+
+if(top!=false){
+    overlay.css('top',xtop+"px");
+}
