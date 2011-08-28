87ec2e5a4db2308d62eb290c46700094a99e8b80
Alex Belozerov <alex@welldonecode.com>
2011-08-28 16:46:32 +0700

    Toggle

--- content.js
+++ content.js
@@ -5,27 +5,33 @@ var left_px = 50;
 var width_px = 300;
 var height_px = 300;
 var zIndex = 1000;
-
 var overlayUniqueId = 'overlay_3985123731465987';
-if ($('#' + overlayUniqueId).length > 0) {
-    //$('#' + overlayUniqueId).remove();
+
+var toggleOverlay = function () {
+    alert('toggleOverlay');
+
+    if ($('#' + overlayUniqueId).length > 0) {
+        $('#' + overlayUniqueId).remove();
+    }
+    else {
+        var overlay = $('<div></div>');
+        overlay.attr({
+            'id': overlayUniqueId
+        }).css({
+            'z-index': zIndex,
+            'width': width_px + 'px',
+            'height': height_px + 'px',
+            'position': 'absolute',
+            'top': top_px + 'px',
+            'left': left_px + 'px',
+            'background-color': 'red',
+            'opacity': opacity,
+            'display': 'block',
+            'cursor': 'all-scroll'
+        });
+        $('body').append(overlay);
+
+        overlay.draggable();
+    }
 }
-else {    
-    var overlay = $('<div></div>');
-    overlay.attr({
-        'id': overlayUniqueId
-    }).css({
-        'z-index': zIndex,
-        'width': width_px + 'px',
-        'height': height_px + 'px',
-        'position': 'absolute',
-        'top': top_px + 'px',
-        'left': left_px + 'px',
-        'background-color': 'red',
-        'opacity': opacity,
-        'cursor': 'all-scroll'
-    });
-    $('body').append(overlay);
-    
-    overlay.draggable();
-}
+
