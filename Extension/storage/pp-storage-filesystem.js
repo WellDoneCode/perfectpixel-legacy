b94e71285a4465b98c055d4874fb94642a18a5fb
Ilya Stepanov <istepanov@renderedsource.com>
2012-05-11 12:56:56 +0700

    Applied GPL v3 license

--- Extension/storage/pp-storage-filesystem.js
+++ Extension/storage/pp-storage-filesystem.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011-2012 Alex Belozerov, Ilya Stepanov
+ * 
+ * This file is part of PerfectPixel.
+ *
+ * PerfectPixel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PerfectPixel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PerfectPixel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Depend on pp-shared.js (PPOverlay class)
 
 // --------------------------------------------------------------------
