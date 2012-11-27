6acb364bd7b74865bfb0f06517a9cbed23a34dae
Alexey Belozerov <abelozerov@renderedsource.com>
2012-11-28 00:14:22 +0700

    Alt+C hotkey info added to Options page

--- Extension/fancy-settings/source/manifest.js
+++ Extension/fancy-settings/source/manifest.js
@@ -22,7 +22,7 @@ this.manifest = {
             "group": "Mouse and Keyboard",
             "name": "enableHotkeys",
             "type": "checkbox",
-            "label": "Enable Hotkeys<br/><br/>Hotkeys list:<br/> Alt + S - Show/Hide overlay<br/> ↑  - Move overlay up<br/> ↓  - Move overlay down<br/> ← - Move overlay left<br/> → - Move overlay right"
+            "label": "Enable Hotkeys<br/><br/>Hotkeys list:<br/> Alt + S - Show/Hide overlay<br/> Alt + C - Lock/Unlock overlay<br/> ↑  - Move overlay up<br/> ↓  - Move overlay down<br/> ← - Move overlay left<br/> → - Move overlay right"
         }
 //        {
 //            "tab": i18n.get("information"),
