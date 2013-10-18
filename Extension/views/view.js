87516ba2ad73d40753cd1b6fcdc36a03007c7aea
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2013-10-18 21:59:09 +0700

    add button and checkbox only if they aren't exist yet

--- Extension/views/view.js
+++ Extension/views/view.js
@@ -510,16 +510,18 @@ var OverlayItemView = Backbone.View.extend({
     },
 
     render: function() {
-        var checkbox = $('<input type=radio name="chromeperfectpixel-selectedLayer" />');
-        this.$el.append(checkbox);
+        if (this.$el.find('.chromeperfectpixel-delete').size() == 0){
+            var checkbox = $('<input type=radio name="chromeperfectpixel-selectedLayer" />');
+            this.$el.append(checkbox);
+
+            var deleteBtn = ($('<button class="chromeperfectpixel-delete">&#x2718;</button>'));
+            deleteBtn.button(); // apply css
+            this.$el.append(deleteBtn);
+        }
 
         this.model.image.getThumbnailUrlAsync($.proxy(function(thumbUrl) {
             thumbUrl && this.$el.css({'background-image':  'url(' + thumbUrl  + ')'});
         }, this));
-
-        var deleteBtn = ($('<button class="chromeperfectpixel-delete">&#x2718;</button>'));
-        deleteBtn.button(); // apply css
-        this.$el.append(deleteBtn);
 
         return this;
     },
